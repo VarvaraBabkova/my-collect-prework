b24c3c3764ad73e67c41fc7794dcc3f899b36036
Varvara Babkova <varvara.munday@gmail.com>
2019-06-08 17:35:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -7,6 +7,6 @@ def my_collect (array)
   return array
 end
 
-my_collect("Tim Jones", "Tom Smith", "Jim Campagno") do |n|
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |n|
   n.split(" ").first
 end
