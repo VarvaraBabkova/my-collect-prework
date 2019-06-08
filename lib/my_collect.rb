c512b700d20f4e6f92a325be18e20d73c1213eed
Varvara Babkova <varvara.munday@gmail.com>
2019-06-08 17:37:40 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect (array)
   i = 0
   new_ar = []
   while i < array.size
-    new_ar.push(yield (array[i])) 
+    new_ar.push(yield (array[i]))
     i += 1
   end
-  return array
+  return new_ar
 end
 
 arr = ["Tim Jones", "Tom Smith", "Jim Campagno"]
